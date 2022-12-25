a9fa1c07236f2eb3e525b458b874ae423ac323b6
idm1try <dimasavchenko1706@icloud.com>
2022-12-25 13:10:29 +0500

    fix: meta

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -4,10 +4,7 @@ const Meta = () => (
   <Head>
     <meta name='msapplication-TileColor' content='#171717' />
     <meta name='theme-color' content='#171717' />
-    <meta
-      name='description'
-      content='View your info for your ip or search it'
-    />
+    <meta name='description' content='View info for your ip or search it' />
     <link rel='icon' type='image/png' sizes='96x96' href='/favicon.png' />
     <link rel='apple-touch-icon' sizes='180x180' href='apple-touch-icon.png' />
 
@@ -15,7 +12,7 @@ const Meta = () => (
     <meta property='og:title' content='IP Info' />
     <meta
       property='og:description'
-      content='View your info for your ip or search it'
+      content='View info for your ip or search it'
     />
     <meta property='og:url' content='https://ip.idm1try.ru/' />
     <meta property='og:image' content='https://ip.idm1try.ru/og.png' />
