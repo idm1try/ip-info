db0da1ce8a6207ac3da781133b745af85d2ba76b
idm1try <dimasavchenko1706@icloud.com>
2022-12-27 13:16:52 +0500

    chore: improve meta

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -2,19 +2,30 @@ import Head from 'next/head'
 
 const Meta = () => (
   <Head>
-    <meta name='msapplication-TileColor' content='#171717' />
-    <meta name='theme-color' content='#171717' />
+    <meta
+      name='theme-color'
+      content='#171717'
+      media='(prefers-color-scheme: dark)'
+    />
+    <meta
+      name='theme-color'
+      content='#fafafa'
+      media='(prefers-color-scheme: light)'
+    />
     <meta name='description' content='View info for your ip or search it' />
     <link rel='icon' type='image/png' sizes='96x96' href='/favicon.png' />
     <link rel='apple-touch-icon' sizes='180x180' href='apple-touch-icon.png' />
-
+    <meta name='robots' content='follow, index' />
     <meta property='og:type' content='website' />
     <meta property='og:title' content='IP Info' />
+    <meta property='og:site_name' content='IP Info' />
     <meta
       property='og:description'
       content='View info for your ip or search it'
     />
+    <meta property='og:locale' content='en_US' />
     <meta property='og:url' content='https://ip.idm1try.ru/' />
+    <link rel='canonical' href='https://ip.idm1try.ru/' />
     <meta property='og:image' content='https://ip.idm1try.ru/og.png' />
     <meta name='twitter:image' content='https://ip.idm1try.ru/og.png' />
     <meta name='twitter:card' content='summary_large_image' />
