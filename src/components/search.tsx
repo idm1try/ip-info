13d97653371c0ce30666d6217f696731fdbbb091
idm1try <dimasavchenko1706@icloud.com>
2023-01-18 00:07:46 +0500

    chore: update

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,4 +1,3 @@
-import cn from '../lib/classNames'
 import useIpInfo from '../lib/useIpInfo'
 
 const Search = () => {
@@ -8,13 +7,7 @@ const Search = () => {
     <div>
       <input
         placeholder='Enter ip'
-        className={cn(
-          'rounded-lg bg-neutral-200 py-2 px-4',
-          'outline-none transition-colors',
-          'duration-300 placeholder:text-neutral-600',
-          'dark:placeholder:text-neutral-400',
-          'dark:bg-neutral-800'
-        )}
+        className='rounded-lg bg-neutral-200 py-2 px-4 outline-none transition-colors duration-300 placeholder:text-neutral-600 hover:bg-neutral-300/70 active:bg-neutral-300/50 dark:bg-neutral-800 dark:placeholder:text-neutral-400 dark:hover:bg-neutral-700/50 dark:active:bg-neutral-700/70'
         onChange={event => setIp(event.target.value)}
         value={ip}
       />
