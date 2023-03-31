290363cda2bdf0564b36d826e83f0fedf23a0e30
idm1try <dimasavchenko1706@icloud.com>
2023-03-31 20:58:31 +0500

    add cn utility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { cn } from '@/lib/utils'
 import useIpInfo from '../lib/useIpInfo'
 
 const Page = () => {
@@ -14,7 +15,16 @@ const Page = () => {
         spellCheck='false'
         value={ip}
         onChange={event => setIp(event.target.value)}
-        className='rounded-lg border border-neutral-400/30 bg-white py-2 px-4 text-neutral-700 shadow-sm outline-none transition-colors duration-300 placeholder:text-neutral-600 hover:text-neutral-900/50 hover:shadow-sm focus:text-neutral-900/50 focus:shadow-sm dark:border-neutral-500/30 dark:bg-neutral-900 dark:text-neutral-300 dark:placeholder:text-neutral-400 dark:hover:border-neutral-500/50 dark:hover:text-white dark:focus:border-neutral-500/50 dark:focus:text-white'
+        className={cn(
+          'rounded-lg border border-neutral-400/30 bg-white',
+          'py-2 px-4 text-neutral-700 shadow-sm outline-none',
+          'transition-colors duration-300 placeholder:text-neutral-600',
+          'hover:text-neutral-900/50 hover:shadow-sm focus:text-neutral-900/50',
+          'focus:shadow-sm dark:border-neutral-500/30 dark:bg-neutral-900',
+          'dark:text-neutral-300 dark:placeholder:text-neutral-400',
+          'dark:hover:border-neutral-500/50 dark:hover:text-white',
+          'dark:focus:border-neutral-500/50 dark:focus:text-white'
+        )}
       />
       {ipInfo && !isLoading && (
         <div>
